4cde3d137b7929eae53ed1486398795e6660ec0a
taylor123454321 <cheeseils@me.com>
2016-04-18 11:04:57 +1200

    random screwing up niss

--- Depth_to_pointcloud_single_normal_live.m
+++ Depth_to_pointcloud_single_normal_live.m
@@ -8,16 +8,8 @@ for i = 1:20
     depthImage = step(depthDevice);
 end
 
-player = pcplayer([-1.5    1.5],[-1.5    1.5],[0.5 2.5],...
-    'VerticalAxis','y','VerticalAxisDir','down');
-
-xlabel(player.Axes,'X (m)');
-ylabel(player.Axes,'Y (m)');
-zlabel(player.Axes,'Z (m)');
-
-
-taken = 50;
-filter = 10;
+taken = 31;
+filter = 15;
 
 for i = 1:taken
     depthImage(:,:,i) = step(depthDevice);
@@ -42,12 +34,17 @@ for b = 1:taken %trimming minium and maxium length
             if depthImage(i,j,b) < 800
                 depthImage(i,j,b) = 0;
             end
+            if i < 20 || i > (424-20)
+                depthImage(i,j,b) = 0;
+            end
+            if j < 20 || j > (512-20)
+                depthImage(i,j,b) = 0;
+            end
         end
     end
 end
 
 dImage_sum = uint16(zeros(424,512,taken-filter));
-
 
 for b = 1:(taken-filter)  %total elements to be summed
     start = b;
@@ -61,39 +58,37 @@ for b = 1:(taken-filter)  %total elements to be summed
     end
 end
 
-dImage_filtered = zeros(424,512,taken-filter);
+dImage_filtered = uint16(zeros(424,512,taken-filter));
 
 for b = 1:taken-filter
     for i = 1:424     %row
         for j = 1:512 %colom
             for k = 1:filter
-                dImage_filtered(i,j,b) = dImage_sum(i,j,k);
+                dImage_filtered(i,j,b) = dImage_sum(i,j,k)/filter;
             end
         end
     end
 end
 
+for b = 1:taken-filter %trimming minium and maxium length
+    for i = 1:424
+        for j = 1:512
+            if dImage_filtered(i,j,b) > 1300
+                dImage_filtered(i,j,b) = 0;
+            end
+            if dImage_filtered(i,j,b) < 800
+                dImage_filtered(i,j,b) = 0;
+            end
+        end
+    end
+end
 
-frames = 5;
+frames = 5
 max_matches = 500;
 vector_time = zeros(max_matches,frames);
 
 for p = 1:frames
-    x = 0;
-    y = 0;
-    z = 0;
-    xx = 0;
-    yy = 0;
-    zz = 0;
-    u = 0;
-    v = 0;
-    w = 0;
-    uu = 0;
-    vv = 0;
-    ww = 0;
-    normals = 0;
-    
-    ptCloud = pcfromkinect(depthDevice,dImage_filtered(:,:,frame));
+    ptCloud = pcfromkinect(depthDevice,dImage_filtered(:,:,p));
     
     normals = pcnormals(ptCloud);
     
@@ -117,20 +112,6 @@ for p = 1:frames
         end
     end
     
-    % for i = 1:43
-    %     for j = 1:52
-    %         if x(i,j) > 1.5
-    %             x(i,j) = NaN;
-    %         end
-    %         if y(i,j) > 1.5
-    %             y(i,j) = NaN;
-    %         end
-    %         if z(i,j) > 1.5
-    %             z(i,j) = NaN;
-    %         end
-    %     end
-    % end
-    
     xx = x;yy = y;zz = z;
     %     for i = 1:43
     %         for j = 1:52
@@ -143,13 +124,11 @@ for p = 1:frames
     %pcshow(ptCloud)
     %title('Adjusted Normals of Point Cloud')
     %hold on
-    quiver3(xx, yy, zz, u, v, w);
+    %quiver3(xx, yy, zz, u, v, w);
     %surf(xx,yy,zz)
     %view(0,-45)
     
-    A = 0;
     A = zeros(132,3);
-    F = 0;
     F = zeros(132,3);
     
     k = 1;
@@ -158,9 +137,9 @@ for p = 1:frames
             if ~isnan(u(i,j))
                 if ~isnan(v(i,j))
                     if ~isnan(w(i,j))
-                        A(k,1) = u(i,j);
-                        A(k,2) = v(i,j);
-                        A(k,3) = w(i,j);
+                        A(k,1) = u(i,j); % vectors that values
+                        A(k,2) = v(i,j); % A point
+                        A(k,3) = w(i,j); % F location
                         F(k,1) = x(i,j);
                         F(k,2) = y(i,j);
                         F(k,3) = z(i,j);
@@ -171,99 +150,117 @@ for p = 1:frames
         end
     end
     
+    AA(:,:,p) = A;
+    
     leng = length(A);
     
     uu = A(:,1);
     vv = A(:,2);
     ww = A(:,3);
+%         xx = F(:,1);
+%         yy = F(:,2);
+%         zz = F(:,3);
     xx = zeros(leng,1);
     yy = zeros(leng,1);
     zz = zeros(leng,1);
     
-    B = 0;
-    B = eye(leng + 2);
-    k = 2;
-    
-    for i = 1:leng
-        B(k,k) = A(i,2);
-        B(k,k-1) = A(i,1);
-        B(k,k+1) = A(i,3);
-        k = k + 1;
-    end
     
     pcshow(ptCloud)
     hold on
-    %quiver3(xx, yy, zz, uu, vv, ww);
-    %quiver(xx, yy, uu, vv);
+    quiver3(xx, yy, zz, uu, vv, ww);
     
     theta_C = zeros(1,leng);
     theta_D = zeros(1,leng);
     theta_E = zeros(1,leng);
     
-    for i = 1:leng
-        theta_C(i) = atan(A(i,1)/A(i,2));
-        theta_D(i) = atan(A(i,2)/A(i,3));
-        theta_E(i) = atan(A(i,1)/A(i,3));
-    end
+    %     for i = 1:leng
+    %         theta_C(i) = atan(A(i,1)/A(i,2));
+    %         theta_D(i) = atan(A(i,2)/A(i,3));
+    %         theta_E(i) = atan(A(i,1)/A(i,3));
+    %     end
     
-    vector_count = zeros(leng,3);
-    trim_c = 0.07;
-    trim_d = trim_c;
-    trim_e = trim_c;
+    vector_count = zeros(leng,1);
+    trim_1 = 0.015;
+    trim_2 = trim_1;
+    trim_3 = trim_2;
+    m = 1;
     
-    
-    for i = 1:leng
+    for i = 1:leng % searching for matching vectors based on angle
         for j = 1:leng
-            
-            if (theta_C(j)-trim_c > theta_C(i)) || (theta_C(i) < theta_C(j)+trim_c)
-                vector_count(i,1) = vector_count(i,1) + 1;
-            end
-            if theta_D(j)-trim_d > theta_D(i) || theta_D(i) < theta_D(j)+trim_d
-                vector_count(i,2) = vector_count(i,2) + 1;
-            end
-            if theta_E(j)-trim_e > theta_E(i) || theta_E(i) < theta_E(j)+trim_e
-                vector_count(i,3) = vector_count(i,3) + 1;
-            end
-        end
-        %     theta_C(i) = NaN;
-        %     theta_D(i) = NaN;
-        %     theta_E(i) = NaN;
-    end
-    
-    for i = 1:leng
-        for j = 1:3
-            if vector_count(i,j) < 20
-                vector_count(i,j) = NaN;
+            if A(j,1) > (A(i,1)-trim_1) && A(j,1) < (A(i,1)+trim_1)
+                if A(j,2) > (A(i,2)-trim_2) && A(j,2) < (A(i,2)+trim_2)
+                    if A(j,3) > (A(i,3)-trim_3) && A(j,3) < (A(i,3)+trim_3)
+                        if i ~= j
+                            vector_count(i,m) = j;
+                            m = m + 1;
+                        end
+                    end
+                end
             end
         end
+        m = 1;
     end
     
+    [i,width] = size(vector_count);
     
-    k = 1;
+    m = 1;
+    n = 0;
     for i = 1:leng
-        if ~isnan(vector_count(i,2))
-            vector_time(k,p) = vector_count(i,1);
-            k = k + 1;
+        if vector_count(i,1) ~= 0
+            for j = 1:width
+                vector_count_2(m,j) = vector_count(i,j);
+                if vector_count(i,j) ~= 0
+                    n = n + 1;
+                end
+            end
+            vector_match(m,1) = i;
+            matches(i,1) = n;
+            n = 0;
+            m = m + 1;
+        end 
+    end
+    
+    [leng_2,width] = size(vector_count_2);
+    
+    m = 1;
+    vector = zeros(1,5);
+    
+%     for i = 1:leng_2
+%         if vector_count_2(i,width) ~= 0
+%             main_vector_p(m,:) = A(vector_match(i,1),:);
+%             vector(1) = i;
+%             m = m + 1;
+%         end
+%         if vector_count_2(i,width-1) ~= 0 && vector(1) ~= i
+%             main_vector_p(m,:) = A(vector_match(i,1),:);
+%             vector(2) = i;
+%             m = m + 1;
+%         end
+%         if vector_count_2(i,width-2) ~= 0 && vector(1) ~= i  && vector(2) ~= i
+%             main_vector_p(m,:) = A(vector_match(i,1),:);
+%             m = m + 1;
+%         end
+%     end
+        
+    biggest_match = max(matches);
+    cut_off = round(0.7*biggest_match);
+    for i = 1:length(matches);
+        if matches(i,1) < cut_off
+            matches(i,1) = 0;
+        end
+    end
+    
+    m = 1;
+    for i = 1:length(matches)
+        if matches(i,1) ~= 0
+            main_vector_p(m,:) = A(matches(i),:);
+            matches(i);
+            m = m + 1;
         end
     end
 end
 release(depthDevice);
 
-trim = 0.001;
-n = 1;
-for i = 1:length(F)
-    for j = 1:length(F)
-        if (F(i,1)-trim) < F(j,1) && F(i,1)+trim > F(j,1)
-            if F(i,2)-trim < F(j,2) && F(i,2)+trim > F(j,2)
-                if F(i,3)-trim < F(j,3) && F(i,3)+trim > F(j,3)
-                    n = n + 1;
-                end
-            end
-        end
-    end
-end
-
-n
 % total = zeros(1,frames);
 %
 % for i = 1:frames
@@ -279,20 +276,19 @@ n
 % vector_avg
 % mean(vector_avg)
 
-k = 1;
-for i = 1:leng
-    if isnan(vector_count(i,1)) || isnan(vector_count(i,2)) || isnan(vector_count(i,3))
-        A_(k,:) = A(i,:);
-        k = k + 1;
-    end
-end
-xxx = zeros(k-1,1);
-yyy = zeros(k-1,1);
-zzz = zeros(k-1,1);
-
-
+% k = 1;
+% for i = 1:leng
+%     if isnan(vector_count(i,1)) || isnan(vector_count(i,2)) || isnan(vector_count(i,3))
+%         A_(k,:) = A(i,:);
+%         k = k + 1;
+%     end
+% end
+% xxx = zeros(k-1,1);
+% yyy = zeros(k-1,1);
+% zzz = zeros(k-1,1);
 
 % close all
+
 
 
 % scatter(1:leng,vector_count(:,1))
@@ -325,12 +321,23 @@ zzz = zeros(k-1,1);
 
 % quiver3(xxx, yyy, zzz, A_(:,1), A_(:,3), A_(:,2));
 
-grid on
+% grid on
+u = main_vector_p
+for i = 1:length(main_vector_p(:,1))
+    for j = 1:3
+        main_vector_p(i,j) = main_vector_p(i,j)*1.5;
+    end
+end
+ 
 
+mm = zeros(length(main_vector_p(:,1)),1);
 
+quiver3(mm, mm, mm, main_vector_p(:,1), main_vector_p(:,2), main_vector_p(:,3),'black');
 
+% hold off
+% plot(matches)
+matches
 
-
-
+vector_count_2
 
 
