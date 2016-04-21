643ff99587580251597cb9ac104c8bc12c5c074c
taylor123454321 <rct47@Ryans-MacBook-Pro.local>
2016-04-22 10:44:34 +1200

    find_n working

--- find_n.m
+++ find_n.m
@@ -1,4 +1,4 @@
-function [vectors] = find_3(main_vector, directions)
+function [vectors] = find_n(main_vector, directions, corr_value)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,7 +6,9 @@ vectors = zeros(directions,3);
 vectors(1,:) = main_vector(1,:);
 m = 2;
 for i = 1:length(main_vector(:,1))
-    if check_vec_corr(vectors, main_vector(i,:)) == 1
+    if check_vec_corr(vectors, main_vector(i,:), corr_value) == 0
+        % 0 for match
+        % 1 for no match
         vectors(m,:) = main_vector(i,:);
         m = m + 1;
     end
